19bcca9647b5d17bd7d04977258d1e822a25ebb7
surfiniaburger <jdmasciano2@gmail.com>
2023-09-04 01:55:11 +0100

    update

--- LittleLemon/littlelemon/restaurant/views.py
+++ LittleLemon/littlelemon/restaurant/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html',{})
+
+def home_view(request):
+    # Your view logic here...
+    return render(request, 'home.html', {})
